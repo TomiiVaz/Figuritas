781b45caa884df7d2430c8dd4e80acbdcecd78b6
Juanchini23 <jmanchini@alumno.unlam.edu.ar>
2022-05-11 22:13:19 -0300

    actualizacion de controladores, servicios y repos

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioAlbum.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioAlbum.java
@@ -2,6 +2,11 @@ package ar.edu.unlam.tallerweb1.repositorios;
 
 import ar.edu.unlam.tallerweb1.modelo.Figurita;
 
+import java.util.*;
+
 public interface RepositorioAlbum {
     void agregarFigurita(Figurita figu);
+    Figurita getFigurita(Integer codigo);
+    List<Figurita> findAllByNombre(String nombre);
+    List<Figurita> findAllByEquipo(Integer idEquipo);
 }
