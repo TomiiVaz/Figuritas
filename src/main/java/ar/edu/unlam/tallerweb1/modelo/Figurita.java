d89450c5e8d5196de141529ec8b9fa8b0cd92ace
TomiiVaz <eliseovazquez01@gmail.com>
2022-05-19 11:10:54 -0300

    Cambios modelos

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Figurita.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Figurita.java
@@ -10,28 +10,23 @@ public class Figurita {
     private Integer id;
     private String nombre;
 
+    @Enumerated(EnumType.STRING)
+    private Posicion posicion;
+
+    private byte dorsal;
+
+    private String equipo;
+
     private Short año;
 
+    @Enumerated(EnumType.STRING)
     private Rareza rareza;
 
-    @OneToOne
+    @ManyToOne
     private Seleccion seleccion;
 
-    public Rareza getRareza() {
-        return rareza;
-    }
-
-    public void setRareza(Rareza rareza) {
-        this.rareza = rareza;
-    }
-
-    public Seleccion getSeleccion() {
-        return seleccion;
-    }
-
-    public void setSeleccion(Seleccion seleccion) {
-        this.seleccion = seleccion;
-    }
+    @ManyToOne
+    private Album album;
 
     public Integer getId() {
         return id;
@@ -49,11 +44,59 @@ public class Figurita {
         this.nombre = nombre;
     }
 
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    public byte getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(byte dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
     public Short getAño() {
         return año;
     }
 
     public void setAño(Short año) {
         this.año = año;
+    }
+
+    public Rareza getRareza() {
+        return rareza;
+    }
+
+    public void setRareza(Rareza rareza) {
+        this.rareza = rareza;
+    }
+
+    public Seleccion getSeleccion() {
+        return seleccion;
+    }
+
+    public void setSeleccion(Seleccion seleccion) {
+        this.seleccion = seleccion;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
     }
 }
