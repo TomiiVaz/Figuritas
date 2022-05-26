bb98e64133ef3dca9287d739f007b9472d9bd150
TomiiVaz <eliseovazquez01@gmail.com>
2022-05-26 10:20:20 -0300

    Intento de Ediatar

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioAlbumImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioAlbumImpl.java
@@ -2,8 +2,10 @@ package ar.edu.unlam.tallerweb1.repositorios;
 
 import ar.edu.unlam.tallerweb1.modelo.Album;
 import ar.edu.unlam.tallerweb1.modelo.Figurita;
+import ar.edu.unlam.tallerweb1.modelo.Seleccion;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -30,6 +32,24 @@ public class RepositorioAlbumImpl implements RepositorioAlbum {
         final Session session = sessionFactory.getCurrentSession();
         return (List<Album>) session.createCriteria(Album.class)
                 .list();
+    }
+
+    @Override
+    public void editarAlbum(Long albumId, String nombreNuevo) {
+        final Session session = sessionFactory.getCurrentSession();
+
+
+        //Busco la seleccion con la id pasada
+        Seleccion seleccion = (Seleccion) session.createCriteria(Seleccion.class)
+                .add(Restrictions.eq("id",albumId))
+                .uniqueResult();
+
+        //Le cambio el nommbre
+        seleccion.setNombre(nombreNuevo);
+
+        //Lo pongo en la db
+        session.update(seleccion);
+
     }
 
 }
