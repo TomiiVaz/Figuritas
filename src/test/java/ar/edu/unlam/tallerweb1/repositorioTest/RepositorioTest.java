1ab4e029384f8cfe81c2f1735004f886cb106213
TomiiVaz <eliseovazquez01@gmail.com>
2022-06-11 23:38:56 -0300

    Agrego test true para que no de error

--- src/test/java/ar/edu/unlam/tallerweb1/repositorioTest/RepositorioTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/repositorioTest/RepositorioTest.java
@@ -1,6 +1,14 @@
 package ar.edu.unlam.tallerweb1.repositorioTest;
 
 import ar.edu.unlam.tallerweb1.SpringTest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class RepositorioTest extends SpringTest{
+
+    @Test
+    public void algoTest(){
+        assertEquals(true, true);
+    }
 }
